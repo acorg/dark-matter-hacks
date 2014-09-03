b540825fa6ac4ba3ee34a9242c4e3090d69500e3
Barbara Muehlemann <barbara.muhlemann@bluewin.ch>
2014-09-03 16:27:16 +0100

    made lint made check

--- scripts/rocAnalysis.py
+++ scripts/rocAnalysis.py
@@ -17,7 +17,6 @@ import copy
 from dark import conversion
 
 
-
 def _records(blastFilename):
     reader = conversion.JSONRecordsReader(blastFilename)
     for record in reader.records():
@@ -26,16 +25,19 @@ def _records(blastFilename):
 
 def countHits(blastFile, cutoff):
     """
-    Counts the number of reads that hit in each file at each level of sequence identity.
+    Counts the number of reads that hit in each file at each level of
+    sequence identity.
 
     @param blastFile: a file with blast output.
     @param cutoff: a bit score cutoff, reads below that will not be considered.
-    @return: the name of the blastFile and a dictionary, with the level of sequence
-        identity as key and the number of reads with that sequence identity as values
+    @return: the name of the blastFile and a dictionary, with the level of
+        sequence identity as key and the number of reads with that sequence
+        identity as values.
     """
-    readsPerLevel = {'100': 0, '99': 0, '95': 0, '90': 0, '85': 0, '80': 0, '75': 0,
-                     '70': 0, '65': 0, '60': 0, '55': 0, '50': 0, '45': 0, '40': 0,
-                     '35': 0, '30': 0, '25': 0, '20': 0, '15': 0, '10': 0, '5': 0, '0': 0}
+    readsPerLevel = {'100': 0, '99': 0, '95': 0, '90': 0, '85': 0, '80': 0,
+                     '75': 0, '70': 0, '65': 0, '60': 0, '55': 0, '50': 0,
+                     '45': 0, '40': 0, '35': 0, '30': 0, '25': 0, '20': 0,
+                     '15': 0, '10': 0, '5': 0, '0': 0}
     t = blastFile.split('.')[2]
     title = t.split('/')[4]
     records = _records(blastFile)
@@ -53,31 +55,33 @@ def countHits(blastFile, cutoff):
     return title, readsPerLevel
 
 
-# after the above function has been called, all dictionaries must be added into a large dictionary
-# to be fed into calculateFrequencies
+# after the above function has been called, all dictionaries must be added
+# into a large dictionary to be fed into calculateFrequencies
 
 
 def calculateFrequencies(hitDict):
     """
     Calculates true negatives, true positives, false positives,
     false negatives, specificity, false positive rate and true positive rate.
-    
-    @param readsPerLevel: a dictionary of the form: blastn4-545 {'60': 56, '65': 115, ...}
 
-    @return: a dictionary containing the scoringScheme as keys, and dictionaries for
-        tn, tp, fp, fn, spec, fpr, tpr.
+    @param readsPerLevel: a dictionary of the form:
+        blastn4-545 {'60': 56, '65': 115, ...}
+
+    @return: a dictionary containing the scoringScheme as keys,
+        and dictionaries for tn, tp, fp, fn, spec, fpr, tpr.
     """
     # transform the dict for each scoring scheme into a list
     listDict = {}
     for scoringScheme in hitDict:
         l = []
-        for nr in [100, 99, 95, 90, 85, 80, 75, 70, 65, 60, 55, 50, 45, 40, 35, 30, 25, 20, 15, 10, 5, 0]:
+        for nr in [100, 99, 95, 90, 85, 80, 75, 70, 65, 60, 55, 50, 45, 40, 35,
+                   30, 25, 20, 15, 10, 5, 0]:
             l.append(hitDict[scoringScheme][str(nr)])
             listDict[scoringScheme] = l
-    
+
     # calculate frequencies
     frequencies = {}
-    
+
     for scoringScheme in listDict:
         tps = []
         tns = []
@@ -106,7 +110,7 @@ def calculateFrequencies(hitDict):
             tns.append(tn)
             fps.append(fp)
             fns.append(fn)
-            
+
         frequencies[scoringScheme] = {'fprs': fprs, 'tprs': tprs, 'spcs': spcs,
                                       'tps': tps, 'tns': tns, 'fps': fps,
                                       'fns': fns, 'raw': res}
@@ -116,16 +120,18 @@ def calculateFrequencies(hitDict):
 def youdenIndex(frequencies):
     """
     Calculate the youden index.
-    This is a measure for which cutoff should be used. Assumes that the optimal cutoff point
-    is the point on the curve furthest away from the chance line going from (0, 0) to (1, 1).
+    This is a measure for which cutoff should be used. Assumes that the optimal
+    cutoff point is the point on the curve furthest away from the chance
+    line going from (0, 0) to (1, 1).
+
     Formula: max{sensitivity + specificity - 1}
 
-    @param frequencies: frequencies dictionary as returned from calculateFrequencies
+    @param frequencies: frequencies dictionary as returned from
+        calculateFrequencies
 
-    @return: a list of tuples with the coordinates of the point on each roc curve corresponding
-        to the cutoff and a list of cutoffs.
+    @return: a list of tuples with the coordinates of the point on each
+        roc curve corresponding to the cutoff and a list of cutoffs.
     """
-    level = [100, 99, 95, 90, 85, 80, 75, 70, 65, 60, 55, 50, 45, 40, 35, 30, 25, 20, 15, 10, 5, 0]
     youdenCoords = []
     cutoffs = []
     for scoringScheme in frequencies:
@@ -137,7 +143,9 @@ def youdenIndex(frequencies):
             youdens.append(youden)
         youdenIndex = max(youdens)
         cutoff = youdens.index(youdenIndex)
-        element = (y[youdens.index(youdenIndex)], frequencies[scoringScheme]['fprs'][youdens.index(youdenIndex)])
+        element = (y[youdens.index(youdenIndex)],
+                   frequencies[scoringScheme][
+                       'fprs'][youdens.index(youdenIndex)])
         cutoffs.append(cutoff)
         youdenCoords.append(element)
 
@@ -147,13 +155,13 @@ def youdenIndex(frequencies):
 def f1Index(frequencies):
     """
     Calculate the F1 index.
-    This is a measure for which cutoff should be used. Assumes that the optimal cutoff point
-    is the point on the curve closest to (0, 1).
+    This is a measure for which cutoff should be used. Assumes that the
+    optimal cutoff point is the point on the curve closest to (0, 1).
     Formula: min{(1-sensitivity)**2 + (1-specificity)**2}
 
-    @param frequencies: frequencies dictionary as returned from calculateFrequencies
+    @param frequencies: frequencies dictionary as returned from
+        calculateFrequencies.
     """
-    level = [100, 99, 95, 90, 85, 80, 75, 70, 65, 60, 55, 50, 45, 40, 35, 30, 25, 20, 15, 10, 5, 0]
     cutoffs = []
     f1Coords = []
     for scoringScheme in frequencies:
@@ -165,7 +173,9 @@ def f1Index(frequencies):
             f1s.append(f1)
         f1Index = min(f1s)
         cutoff = f1s.index(f1Index)
-        element = (y[f1s.index(f1Index)], frequencies[scoringScheme]['fprs'][f1s.index(f1Index)])
+        element = (y[f1s.index(f1Index)],
+                   frequencies[scoringScheme][
+                       'fprs'][f1s.index(f1Index)])
         cutoffs.append(cutoff)
         f1Coords.append(element)
 
@@ -175,11 +185,13 @@ def f1Index(frequencies):
 def areaUnderCurve(frequencies):
     """
     Calculate the area under each curve.
-    This acts as a measure for how good the test is at distinguishing between tp and tn.
-    The larger the area under the curve (the closer to 1), the better the test. The closer
-    the area to 0.5 the more rubbish is the test.
+    This acts as a measure for how good the test is at distinguishing
+    between tp and tn.
+    The larger the area under the curve (the closer to 1), the better the test.
+    The closer the area to 0.5 the more rubbish is the test.
 
-    @param frequencies: frequencies dictionary as returned from calculateFrequencies
+    @param frequencies: frequencies dictionary as returned from
+        calculateFrequencies.
     """
     freqs = copy.copy(frequencies)
     integrated = {}
@@ -200,12 +212,13 @@ def areaUnderCurve(frequencies):
 
 def areaUnderCurveIndex(integrated):
     """
-    Returns a dictionary where the key is a scoringScheme, and the value goes from 0, 1, 2...
-    in the order of area under curve.
+    Returns a dictionary where the key is a scoringScheme, and
+    the value goes from 0, 1, 2... in the order of area under curve.
 
     @param integrated: result from areaUnderCurve
     """
-    sortedIntegrated = sorted(integrated.iteritems(), key=operator.itemgetter(1))[::-1]
+    sortedIntegrated = sorted(integrated.iteritems(),
+                              key=operator.itemgetter(1))[::-1]
 
     indexes = {}
     for i, item in enumerate(sortedIntegrated):
